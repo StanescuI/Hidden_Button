0d0b95d569adf89fb2409176a22c90289f9ef71f
StanescuI <162870985+StanescuI@users.noreply.github.com>
2024-03-14 18:03:14 +0200

    Made separate javascript and css files

---
+++ script.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll('.button');
+        const randomIndex = Math.floor(Math.random() * buttons.length);
+        const winningButton = buttons[randomIndex];
+
+        function revealResult(currentButton) {
+            if (currentButton === winningButton) {
+                alert("Winner");
+            } else {
+                alert("Loser");
+            }
+        }
