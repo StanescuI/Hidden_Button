8b5ffdee35c43ae14eb7e500a6c73bf94cc860d1
StanescuI <162870985+StanescuI@users.noreply.github.com>
2024-03-17 09:55:33 +0200

    Fixed indentation in script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const buttons = document.querySelectorAll('.button');
-        const randomIndex = Math.floor(Math.random() * buttons.length);
-        const winningButton = buttons[randomIndex];
-
-        function revealResult(currentButton) {
-            if (currentButton === winningButton) {
-                alert("Winner");
-            } else {
-                alert("Loser");
-            }
-        }
+const randomIndex = Math.floor(Math.random() * buttons.length);
+const winningButton = buttons[randomIndex];
+function revealResult(currentButton) {
+    if (currentButton === winningButton) {
+        alert("Winner");
+    } else {
+        alert("Loser");
+    }
+}
